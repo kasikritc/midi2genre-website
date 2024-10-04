512f7f375a39c4dbea2287341d1d797a3b5f9b66
kasikritc <kasikrit.c@outlook.com>
2024-10-04 13:27:12 -0400

    Updated style.css location

--- proposal-streamlit/app.py
+++ proposal-streamlit/app.py
@@ -7,7 +7,7 @@ def local_css(file_name):
     with open(file_name, "r") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("./style.css")
 
 # Particle animation background
 def particle_background():
