7ac8c0403b1cc33b305e0f3f15e6ac212d75d86f
kasikritc <kasikrit.c@outlook.com>
2024-12-03 17:10:38 -0500

    Final report website adjusted

--- proposal-streamlit/app.py
+++ proposal-streamlit/app.py
@@ -144,6 +144,20 @@ def main():
     The second machine learning algorithm we decided to implement is a Support Vector Machine (SVM), once again using our final feature set. Some of the processing we did to take the chord progressions and turn it into features resulted in a rather large feature matrix (currently at about 101 features) and this was after using the PCA algorithm on a couple of the heftier features. Because of this larger feature set, we needed an algorithm that would be able to handle this and SVM is thus a great choice. SVM is able to handle all these features (and large data sets in general) on lower computational power due to its memory efficiency [7]. This memory efficiency comes from the model using support vectors that are formed from a subset of the input data and used in the decision function. This algorithm is also useful due to the kernel functions we can modify: similar to the random forest, if the relationship between our features and the genre classifications is non-linear, the kernel can transform the data into a hyperplane, allowing for more accurate classification and maintaining a linear divide in the data. We chose to use the RBF kernel for our training due to the non-linearity nature of our data.
     """)
 
+    st.markdown("<h3 class='subsection-title'>K-Nearest Neighbors</h3>", unsafe_allow_html=True)
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <img src="https://miro.medium.com/v2/resize:fit:640/format:webp/0*2_qzcm2gSe9l67aI.png" alt="Introduction Image" style="width:100%; height:auto;">
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
+    st.markdown("Picture Credit: researchgate.net")
+    st.write("""
+    The third machine learning algorithm we decided to implement is a K-Nearest-Neighbors algorithm, using the final feature set like our other algorithms. KNN fits our problem because it doesn’t assume any type of linear relationship between the data and the output, allowing for flexibility within the processing, be it linear or complex patterns. KNN also inherently deals with multiclass classification, something we clearly need to distinguish between multiple different music genres. Our dataset also implies some similarity between the pop and rock chord progressions, with fuzzy differences to the naked eye. However, KNN can adapt to these and deal with complicated decision boundaries. It does this by using proximity metrics. It takes the closest neighbors in the feature space and gives more weight to those data points to accurately classify and cluster the data even if there’s overlap. All these reasons make KNN an effective choice for our specific problem.
+    """)
+    
     # Results and Discussion section
     st.markdown("<div class='section-container orange-gradient'>", unsafe_allow_html=True)
     st.markdown("<h2 class='section-title'>Results and Discussion</h2>", unsafe_allow_html=True)
@@ -233,9 +247,64 @@ def main():
     st.write("""
     The confusion matrix reveals significant classification challenges in the model's performance. Pop and Rock show the highest number of correct predictions (4006 and 3551 respectively), but there's substantial confusion between these two genres, with 1821 Pop songs misclassified as Rock and 2345 Rock songs misclassified as Pop. World music also shows a decent number of correct predictions (2624), but experiences considerable confusion with Pop and Rock genres. Most notably, Classical, Electronic, Folk, and Hiphop genres show extremely poor performance with almost no correct predictions, with their samples being predominantly misclassified as either Pop or Rock. This pattern suggests the model has a strong bias toward the two dominant classes (Pop and Rock), likely due to their larger representation in the training data.
     """)
-    
+
+    st.markdown("<h3 class='subsection-title'>KNN Results</h3>", unsafe_allow_html=True)
+    local_pic("ConfusionMatrixKNN.png", caption="Figure 5: Confusion Matrix for KNN")
+
+    st.write("""The KNN model performed the best in recognizing Jazz, Pop, Rock, and World, which were the most occurring in the dataset. These genres have the highest number of correct predictions as shown in the confusion matrix, where the predicted class matches the true class. In terms of correct classifications, Pop had 3,942, Rock had 2,987, and World had 2,996. This shows that the model learned to recognize features associated these genres the best. Unfortunately, the matrix also shows a lot of confusion between the genres, as the shading around misclassifications surrounding those genres is darker.
+    """)
+
+    st.write("""Pop, Rock, and World genres were often misclassified for one another. In the confusion matrix, the shading highlights where Rock was confused with Pop and vice versa and World was confused with both Pop and Rock. This implies that the model struggles to differentiate these genres, most likely due to overlapping features, since the genres use similar chord progressions and styles. The lack of distinction between these genres, despite their high representation, likely lead to features that were also non-distinct, making it harder for the model to differentiate them.
+    """)
+
+    knn_quant_metrics = {
+        "Genre": ["Classical", "Country", "Electronic", "Folk", "Hiphop", "Jazz", "Pop", "Rock", "Soul", "World"],
+        "Precision": [0.14, 0.33, 0.20, 0.22, 0.32, 0.56, 0.39, 0.42, 0.37, 0.41],
+        "Recall": [0.01, 0.22, 0.00, 0.01, 0.01, 0.53, 0.55, 0.42, 0.05, 0.50],
+        "F1-Score": [0.03, 0.26, 0.01, 0.02, 0.02, 0.55, 0.46, 0.42, 0.08, 0.45],
+        "Support": [71, 1916, 271, 1416, 634, 922, 7179, 7060, 1430, 6049]
+    }
+    st.table(pd.DataFrame(knn_quant_metrics))
+    st.markdown(
+        """
+        <style>
+        .dataframe {
+            color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown("<p style='text-align: center; font-size: 0.9em; color: gray;'>Figure 6: Qualitative Metrics for KNN Model</p>", unsafe_allow_html=True)
+
+    st.write("""The F1-score is a good representation of both the precision and recall the model has for each genre. Jazz(0.55), Pop (0.46), World (0.45), and Rock (0.42) all had higher F1-scores which suggests that the model is more effective at predicting these classes, likely due to their overrepresentation and also Jazz’s distinct nature musically. This allowed the model to learn more subtle pattern differences, leading to higher confidence in predictions.
+             \nWhen looking at genres like Soul, Classical, Folk, and Electronic we see extremely low F1-scores (0.08, 0.03, 0.02, and 0.01, respectively), showing the model’s struggle with these classes likely due to how underrepresented they are in the dataset. Low recall for these genres, such as Classical, Folk, Hip-Hop, and Electronic (0.01, 0.01, 0.01, and 0.00), also highlights that the model rarely identifies these classes, exposing the model’s inability to notice clear patterns for minority genres. There was also relatively high precision but near-zero recall for these genres which reveals that the model predicts them fairly correctly when it does, but rarely makes those predictions due to their sparseness in the dataset, impacting the F1-scores. It's clear the KNN model does better with the genres we had more to train on, and still requires some more advanced feature engineering to distinguish between them.
+    """)
+
+    st.markdown("<h3 class='subsection-title'>Looking at All Models by F1 Scores</h3>", unsafe_allow_html=True)
+    all_summary_metrics = {
+        "Genre": ["Classical", "Country", "Electronic", "Folk", "Hip-Hop", "Jazz", "Pop", "Rock", "Soul", "World"],
+        "KNN": [0.03, 0.26, 0.01, 0.02, 0.02, 0.55, 0.46, 0.42, 0.08, 0.45],
+        "Random Forest": [0.03, 0.26, 0.01, 0.02, 0.02, 0.55, 0.46, 0.42, 0.08, 0.45],
+        "SVM": [0.00, 0.12, 0.00, 0.00, 0.00, 0.57, 0.46, 0.44, 0.02, 0.43]
+    }
+    st.table(pd.DataFrame(all_summary_metrics))
+    st.markdown(
+        """
+        <style>
+        .dataframe {
+            color: white;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown("<p style='text-align: center; font-size: 0.9em; color: gray;'>Figure 7: F1 Scores for Three Models Trained</p>", unsafe_allow_html=True)
+
+    st.write("""This project evaluated three machine learning models—K-Nearest Neighbors (KNN), Random Forest, and Support Vector Machine (SVM)—for classifying musical genres based on chord progressions. KNN performed best, achieving higher F1-scores for genres like Jazz, Pop, Rock, and World due to its adaptability to complex, non-linear feature distributions. However, it struggled with underrepresented genres like Classical and Electronic, reflecting its sensitivity to class imbalances. Random Forest, while effective for majority genres and strong in feature interpretation, suffered from significant genre confusion, particularly between overlapping genres such as Pop and Rock. SVM, despite its computational efficiency with an RBF kernel, performed poorly overall, especially for minority genres, due to its inability to handle class imbalances and subtle feature variations.
+    \nConfusion matrices revealed consistent difficulties in distinguishing between Pop, Rock, and World genres across all models. Genre confusion was most pronounced in Random Forest, while KNN showed better adaptability but remained constrained by dataset imbalances. SVM’s reliance on sparse data representations resulted in near-zero recall for underrepresented genres. Overall, KNN proved the strongest for majority genres, though its limitations, along with those of the other models, underscore the need for enhanced data balancing and feature engineering. Future improvements could include synthetic data generation, exploring additional harmonic and rhythmic features, and integrating deep learning models like LSTMs to better capture sequential patterns in chord progressions.""")
     st.markdown("<h3 class='subsection-title'>Further Analysis</h3>", unsafe_allow_html=True)
-    local_pic("features-pca.png", caption="Figure 5: 2D PCA Plot of Our Features")
+    local_pic("features-pca.png", caption="Figure 8: 2D PCA Plot of Our Features")
     st.write("""
     We performed Principal Component Analysis (PCA) on the dataset to examine the variability and potential genre clusters based on our features. The results indicate a lack of variance in the principal components, suggesting that the features do not effectively distinguish genres.
 
@@ -248,14 +317,21 @@ def main():
     """)
 
     st.markdown("<div class='section-container purple-gradient'>", unsafe_allow_html=True)
-    st.markdown("<h2 class='section-title'>Next Steps</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 class='section-title'>Future Work</h2>", unsafe_allow_html=True)
 
-    st.write("""
-    To improve model performance (both for SVM and Random Forest), we will prioritize feature engineering. By identifying and incorporating more genre-distinctive features, we aim to enhance the models’ ability to capture meaningful patterns across genres and address the limitations identified in our initial analysis.""")
-    
-    st.write("""Specifically, we plan to explore an embedding model [1] that treats chord progressions as a unique “language,” leveraging a pretrained language model fine-tuned on chord sequences to capture genre-related features. This approach will help create embeddings that represent each song’s musical characteristics. We will feed the chord embeddings as sequences into the model to train it on genre prediction. To reduce model complexity, we’ll apply PCA to compress the embeddings to around 50 dimensions. Additionally, we will experiment with averaging chord embeddings as a baseline and compare this approach with the sequence model to evaluate which better distinguishes genres. These steps aim to refine our feature representation and improve genre classification, especially for underrepresented genres.
+    st.write("""Although we were able to do some work on engineering the features of the engineering model, for the timing of this project we were not able to fully implement the chord embedding model. In the future, we would like to delve into how this different representation of chords will impact the accuracy of the 3 models for classifying genres.""")
+    st.markdown("<h3 class='subsection-title'>Chord Embeddings as Language</h3>", unsafe_allow_html=True)
+    local_pic("lstm-architecture.webp", caption="Figure 9: LSTM Architecture that could be used to train for genre classification")
+
+    st.write("""Chord embeddings can be treated like a language due to their sequential structure, similar to how words form sentences. Transformer models, such as those used in symbolic music generation, can be used for processing sequential data like chord progressions. By leveraging self-attention mechanisms, these models can capture long-term dependencies and context within sequences. For example, pretrained transformer models like BERT or GPT can be fine-tuned on chord sequences to learn relationships specific to musical genres, building on techniques shown to enhance symbolic music processing [8][9].
+    \nFor the sake of simplicity, models like Word2Vec or FastText could create embeddings based on harmonic relationships, although these may lack the contextual depth of transformers. Alternatively, recurrent neural networks (RNNs) like LSTMs or GRUs can account for temporal dependencies in chord sequences, capturing the influence of earlier chords on later ones [9].
     """)
-    local_pic("chord-embedding-model-preview.png", caption="Figure 6: Preview of the Chord Embedding Model. In (a), we show the circle of fifths with the same colors as in (b), (c) and (d), which show the same 2-dimensional PCA projection of the chord embedding space with lines denoting the circle of fifths over major chords (b) and minor chords (c), and lines denoting major-minor relatives (d).")
+
+    st.markdown("<h3 class='subsection-title'>Chord Embeddings as Graphs</h3>", unsafe_allow_html=True)
+    local_pic("chord-graphs.jpg", caption="Figure 10: A possible diagram of treating chord as graphs")
+    st.write("""Another approach would involve representing chord embeddings as graphs, where nodes represent unique chords and edges represent transitions between chords, weighted by their frequency or context. Graph-based approaches have been successfully used in tasks like melody-to-chord harmonization, where structural patterns in music are important. For example, integrating Graph Neural Networks (GNNs) to learn chord relationships could help model genre-specific motifs such as secondary dominants in jazz or cyclic structures in pop[9]. GNNs can be enhanced by incorporating additional features, such as edge Directionality (capturing the difference between A → D major and D → A major transitions) and embedding nodes with harmonic intervals and frequency distributions.
+    \nGraph-based embeddings, combined with sequence models like transformers, could offer a hybrid solution to address both structural and temporal aspects of chord progressions, making this a promising area for further research [8][9].
+    """)
     st.markdown("</div>", unsafe_allow_html=True)
 
     # References section
@@ -268,7 +344,9 @@ def main():
         "Chords and Lyrics Dataset, Kaggle.",
         "Lakh MIDI Dataset, Kaggle.",
         "AIML.com. (2024, May 22). What are the advantages and disadvantages of Random Forest?",
-        "Scikit-Learn Developers. (n.d.). Support vector machines. Scikit-Learn."
+        "Scikit-Learn Developers. (n.d.). Support vector machines. Scikit-Learn.",
+        "N. Zhang, \"Learning Adversarial Transformer for Symbolic Music Generation,\" in IEEE Transactions on Neural Networks and Learning Systems, vol. 34, no. 4, pp. 1754-1763, April 2023, doi: 10.1109/TNNLS.2020.2990746.",
+        "S. Rhyu, H. Choi, S. Kim and K. Lee, \"Translating Melody to Chord: Structured and Flexible Harmonization of Melody With Transformer,\" in IEEE Access, vol. 10, pp. 28261-28273, 2022, doi: 10.1109/ACCESS.2022.3155467"
     ]
     for i, ref in enumerate(references, 1):
         st.markdown(f"<p class='reference-item'>[{i}] {ref}</p>", unsafe_allow_html=True)
@@ -282,21 +360,20 @@ def main():
     )
     st.markdown("Picture Credit: images.coolwallpapers.me")
     st.markdown("</div>", unsafe_allow_html=True)
-    
 
     st.markdown("<div class='section-container green-gradient'>", unsafe_allow_html=True)
     st.markdown("<h2 class='section-title'>Project Management</h2>", unsafe_allow_html=True)
 
-    local_pic("ml-14-midterm-gantt-chart.png", caption="Figure 7: Current Progress of Gantt Chart")
+    local_pic("ml-14-midterm-gantt-chart.png", caption="Figure 11: Gantt Chart used for this project")
     
     contributions_table = {
         "Name": ["Thomas", "Hannah", "Kasikrit", "Michaelah", "Tooni"],
         "Contribution": [
-            "Gantt Chart, Introduction, Methods - Random forest",
-            "Methods - SVM",
-            "Website Creation, Results/Discussion - SVM",
-            "Results/Discussion - SVM",
-            "Data Processing Methods"
+            "Gantt Chart, Data preprocessing/feature engineering, Github file directory, Video",
+            "Methods - KNN, Video",
+            "Website Creation using stremlit, Comparison between all the models",
+            "Future work",
+            "Data Processing Methods KNN, Video"
         ]
     }
     st.table(pd.DataFrame(contributions_table))
